ca15a2f8ae351f14db5c2d578c54388de509602a
Dakyung Seo <seoda0000@gmail.com>
2023-04-18 10:45:30 +0900

    Chore: GSON, Retrofit2

--- front-end/groot/app/src/main/java/com/chocobi/groot/LoginActivity.kt
+++ front-end/groot/app/src/main/java/com/chocobi/groot/LoginActivity.kt
@@ -2,10 +2,31 @@ package com.chocobi.groot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var loginIdInput = findViewById<EditText>(R.id.loginIdInput)
+        var loginPwInput = findViewById<EditText>(R.id.loginPwInput)
+        val basicLoginBtn = findViewById<Button>(R.id.basicLoginBtn)
+        basicLoginBtn.setOnClickListener {
+            var textId = loginIdInput.text.toString()
+            var textPw = loginPwInput.text.toString()
+
+            var dialog = AlertDialog.Builder(this)
+            dialog.setTitle("알림!")
+            dialog.setMessage("id:" + textId + "pw:" + textPw)
+            dialog.show()
+
+        }
+
+
+
     }
 }
