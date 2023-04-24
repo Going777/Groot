2e7228b1962d0ae17026650b6f10bc0afa2fce64
sehee <parksehee6938@gmail.com>
2023-04-24 15:19:48 +0900

    [FE]feat: createArticle form

--- front-end/groot/app/src/main/java/com/chocobi/groot/view/community/CommunityPostFragment.kt
+++ front-end/groot/app/src/main/java/com/chocobi/groot/view/community/CommunityPostFragment.kt
@@ -1,12 +1,15 @@
 package com.chocobi.groot.view.community
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil.setContentView
 import com.chocobi.groot.R
 import retrofit2.Call
@@ -22,6 +25,13 @@ class CommunityPostFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_community_post, container, false)
         val toPostListBtn = view.findViewById<Button>(R.id.toPostListBtn)
 
+        var titleCnt = view.findViewById<TextView>(R.id.titleCnt)
+        var contentCnt = view.findViewById<TextView>(R.id.contentCnt)
+
+        var titleCntValue = 0
+        var contentCntValue = 0
+
+
         //        retrofit 객체 만들기
         var retrofit = Retrofit.Builder()
             .baseUrl("http://10.0.2.2:8000")
@@ -30,12 +40,26 @@ class CommunityPostFragment : Fragment() {
 
         var communityPostService = retrofit.create(CommunityPostService::class.java)
 
+        val titleInput = view.findViewById<EditText>(R.id.titleInput).toString()
+        val contentInput = view.findViewById<EditText>(R.id.contentInput).toString()
+
+//        fun textWatcher() {
+//            titleInput.addTextChangedListener(object: TextWatcher {
+//                override fun afterTextChanged(s: Editable?) {
+//                    titleCntValue++
+//                }
+//                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//                    // 입력하기 전에 호출됩니다.
+//                }
+//                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                    // 입력 중에 호출됩니다.
+//                }
+//            })
+//        }
+
 
     // 등록 버튼 클릭 시 제목과 내용 입력값
         toPostListBtn.setOnClickListener(View.OnClickListener{
-            val titleInput = view.findViewById<EditText>(R.id.titleInput).toString()
-            val contentInput = view.findViewById<EditText>(R.id.contentInput).toString()
-
             // 요청 보내기
             communityPostService.requestLogin(titleInput, contentInput).enqueue(object:
                 Callback<CommunityPost>{
