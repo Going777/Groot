18c25a8b07738ebb3c45a90c3816d98fb717efbe
김정원 <=>
2023-04-26 10:18:11 +0900

    Feat: Weather Fragment 추가

--- front-end/groot/app/src/main/java/com/chocobi/groot/ShortcutActivity.kt
+++ front-end/groot/app/src/main/java/com/chocobi/groot/ShortcutActivity.kt
@@ -1,105 +1,17 @@
 package com.chocobi.groot
 
-import android.Manifest
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import com.chocobi.groot.mlkit.kotlin.ml.ArActivity
 import com.chocobi.groot.view.login.LoginActivity
 import com.chocobi.groot.view.signup.SignupActivity
-import com.chocobi.groot.view.weather.WeatherFragment
-import com.google.gson.annotations.SerializedName
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Query
 
 class ShortcutActivity : AppCompatActivity() {
-
-    companion object {
-        var BaseUrl = "http://api.openweathermap.org/"
-        var AppId = "faf492ab03cfcf1c5cd016bad32edb7b"
-        var lat = "37.445293"
-        var lon = "126.785823"
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shortcut)
-
-        val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val isGPSEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
-        val isNetworkEnabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this, arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
-                WeatherFragment.WEATHER_REQUEST
-            )
-            return
-        }
-
-        //Create Retrofit Builder
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BaseUrl)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val service = retrofit.create(WeatherService::class.java)
-        val call = service.getCurrentWeatherData(lat, lon, AppId)
-
-        val textView = findViewById<TextView>(R.id.textView)
-        call.enqueue(object : Callback<WeatherResponse> {
-            override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
-                Log.d("MainActivity", "result :" + t.message)
-            }
-
-            override fun onResponse(
-                call: Call<WeatherResponse>,
-                response: Response<WeatherResponse>
-            ) {
-                if (response.code() == 200) {
-                    val weatherResponse = response.body()
-                    Log.d("MainActivity", "result: " + weatherResponse.toString())
-                    var cTemp = weatherResponse!!.main!!.temp - 273.15  //켈빈을 섭씨로 변환
-                    var minTemp = weatherResponse!!.main!!.temp_min - 273.15
-                    var maxTemp = weatherResponse!!.main!!.temp_max - 273.15
-                    val stringBuilder =
-                        "지역: " + weatherResponse!!.sys!!.country + "\n" +
-                                "현재기온: " + cTemp + "\n" +
-                                "최저기온: " + minTemp + "\n" +
-                                "최고기온: " + maxTemp + "\n" +
-                                "풍속: " + weatherResponse!!.wind!!.speed + "\n" +
-                                "일출시간: " + weatherResponse!!.sys!!.sunrise + "\n" +
-                                "일몰시간: " + weatherResponse!!.sys!!.sunset + "\n" +
-                                "아이콘: " + weatherResponse!!.weather!!.get(0).icon + "\n" +
-                                "위경도: " + lat + "," + lon
-
-                    textView.text = stringBuilder
-                }
-            }
-
-        })
 
         val shortcutLogin = findViewById<Button>(R.id.shortcutLogin)
         val shortcutSignup = findViewById<Button>(R.id.shortcutSignup)
@@ -131,82 +43,5 @@ class ShortcutActivity : AppCompatActivity() {
             var intent = Intent(this, ArActivity::class.java)
             startActivity(intent)
         }
-
     }
-}
-
-interface WeatherService {
-
-    @GET("data/2.5/weather")
-    fun getCurrentWeatherData(
-        @Query("lat") lat: String,
-        @Query("lon") lon: String,
-        @Query("appid") appid: String
-    ):
-            Call<WeatherResponse>
-}
-
-class WeatherResponse() {
-    @SerializedName("weather")
-    var weather = ArrayList<Weather>()
-
-    @SerializedName("main")
-    var main: Main? = null
-
-    @SerializedName("wind")
-    var wind: Wind? = null
-
-    @SerializedName("sys")
-    var sys: Sys? = null
-}
-
-class Weather {
-    @SerializedName("id")
-    var id: Int = 0
-
-    @SerializedName("main")
-    var main: String? = null
-
-    @SerializedName("description")
-    var description: String? = null
-
-    @SerializedName("icon")
-    var icon: String? = null
-}
-
-class Main {
-    @SerializedName("temp")
-    var temp: Float = 0.toFloat()
-
-    @SerializedName("humidity")
-    var humidity: Float = 0.toFloat()
-
-    @SerializedName("pressure")
-    var pressure: Float = 0.toFloat()
-
-    @SerializedName("temp_min")
-    var temp_min: Float = 0.toFloat()
-
-    @SerializedName("temp_max")
-    var temp_max: Float = 0.toFloat()
-
-}
-
-class Wind {
-    @SerializedName("speed")
-    var speed: Float = 0.toFloat()
-
-    @SerializedName("deg")
-    var deg: Float = 0.toFloat()
-}
-
-class Sys {
-    @SerializedName("country")
-    var country: String? = null
-
-    @SerializedName("sunrise")
-    var sunrise: Long = 0
-
-    @SerializedName("sunset")
-    var sunset: Long = 0
 }
--- front-end/groot/app/src/main/java/com/chocobi/groot/view/weather/WeatherFragment.kt
+++ front-end/groot/app/src/main/java/com/chocobi/groot/view/weather/WeatherFragment.kt
@@ -32,113 +32,43 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class WeatherFragment : Fragment() {
-
-    companion object {
-        const val API_KEY: String = "faf492ab03cfcf1c5cd016bad32edb7b"
-        const val WEATHER_URL: String = "https://api.openweathermap.org/data/2.5/weather"
-        const val MIN_TIME: Long = 5000
-        const val MIN_DISTANCE: Float = 1000F
-        const val WEATHER_REQUEST: Int = 102
-    }
-
-//    private var binding: FragmentStartBinding?= null
-    private lateinit var weatherState: TextView
-    private lateinit var temperature: TextView
-    private lateinit var weatherTip: TextView
-    private lateinit var weatherIcon: ImageView
-
-    private lateinit var mLocationManager: LocationManager
-    private lateinit var mLocationListener: LocationListener
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_weather, container, false)
-    //        val fragmentBinding = FragmentStartBinding.inflate(inflater, container, false)
-//        binding = fragmentBinding
-//        return fragmentBinding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-//        binding?.startFragment = this
-//        binding?.apply {
-//            temperature = temperatureTv
-//            weatherState = weatherTv
-//            weatherTip = weatherTipTv
-//            weatherIcon = weatherIc
-//        }
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        getWeatherInCurrentLocation()
-    }
-
-    private fun getWeatherInCurrentLocation(){
-        mLocationManager = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-        mLocationListener = LocationListener { p0 ->
-            val params: RequestParams = RequestParams()
-            params.put("lat", p0.latitude)
-            params.put("lon", p0.longitude)
-            params.put("appid", API_KEY)
-            doNetworking(params)
-        }
-
-
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(requireActivity(), arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION), WEATHER_REQUEST)
-            return
-        }
-        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, mLocationListener)
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, mLocationListener)
-    }
-
-
-    private fun doNetworking(params: RequestParams) {
-        var client = AsyncHttpClient()
-
-        client.get(WEATHER_URL, params, object: JsonHttpResponseHandler(){
-            override fun onSuccess(
-                statusCode: Int,
-                headers: Array<out Header>?,
-                response: JSONObject?
-            ) {
-                val weatherData = WeatherData().fromJson(response)
-                if (weatherData != null) {
-                    updateWeather(weatherData)
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment BlankFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            WeatherFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
             }
-
-        })
-    }
-
-    private fun updateWeather(weather: WeatherData) {
-        temperature.setText(weather.tempString+" ℃")
-        weatherState.setText(weather.weatherType)
-        val resourceID = resources.getIdentifier(weather.icon, "drawable", activity?.packageName)
-        weatherIcon.setImageResource(resourceID)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if(mLocationManager!=null){
-            mLocationManager.removeUpdates(mLocationListener)
-        }
     }
 }
