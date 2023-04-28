1b38faab2de2fb2810b3167c635a16973c42c38d
bkkmw <roboticts49@gmail.com>
2023-04-28 17:52:33 +0900

    Feat: get code data

--- plant-data/garden_details.py
+++ plant-data/garden_details.py
@@ -13,13 +13,13 @@ filename = os.environ.get('OUTPUT_DIR') + 'garden_list.csv'
 fr = open(filename, 'r', encoding='utf-8-sig')
 reader = csv.reader(fr)
 
-# filename = 'garden_details_code.csv'
-filename = os.environ.get('OUTPUT_DIR') + 'garden_details_name.csv'
+filename = os.environ.get('OUTPUT_DIR') + 'garden_details_code.csv'
+# filename = os.environ.get('OUTPUT_DIR') + 'garden_details_name.csv'
 fw = open(filename, 'w', encoding='utf-8-sig', newline='')
 writer = csv.writer(fw)
 
 # output columns
-col = ['id', 'kr_name', 'en_name', 'bo_name', 'advise', 'height', 'area',
+col = ['id', 'kr_name', 'bo_name', 'advise', 'height', 'area',
        'smell', 'toxic', 'mgmt_level', 'grw_speed', 'grw_temp', 'humidity',
        'water_cycle', 'water_cycle_winter', 'special_mgmt', 'functionalities',
        'mgmt_demand', 'leaf_pattern', 'leaf_color', 'light_demand',
@@ -27,7 +27,7 @@ col = ['id', 'kr_name', 'en_name', 'bo_name', 'advise', 'height', 'area',
 writer.writerow(col)
 
 # output columns description
-col_kr = ['id', '한글명', '영문명', '학명', 'ㅁㄹ..', '높이', '넓이',
+col_kr = ['id', '한글명', '학명', 'ㅁㄹ..', '높이', '넓이',
           '냄새', '독성', '관리 레벨', '성장 속도', '성장 온도', '습도',
           '물 주기', '겨울 물 주기', '관리 특이사항', '기능성',
           '관리 요구', '잎 패턴', '잎 색상', '빛 요구량',
@@ -53,25 +53,13 @@ for row in reader:
         soup = BeautifulSoup(html, 'html.parser')
 
         # code
-        # data = [id, name, soup.find('plntzrnm').text, soup.find('plntbnenm').text,
-        #     soup.find('adviseinfo').text, soup.find('growthhginfo').text, soup.find('growtharainfo').text,
-        #     soup.find('smellcode').text, soup.find('toxctyinfo').text, soup.find('managelevelcode').text,
-        #     soup.find('grwtvecode').text, soup.find('grwhtpcode').text, soup.find('hdcode').text,
-        #     soup.find('watercyclesprngcode').text, soup.find('watercyclewintercode').text,
-        #     soup.find('speclmanageinfo').text, soup.find('fncltyinfo').text,
-        #     soup.find('managedemanddocode').text, soup.find('lefmrkcodenm').text,
-        #     soup.find('lefcolrcodenm').text, soup.find('lighttdemanddocodenm').text
-        #     soup.find('postngplacecodenm').text, soup.find('dlthtscodenm').text, soup.find('grwhstlecode').text
-        #     soup.find('winterlwettpcode').text]
-
-        # name
-        data = [id, name, soup.find('plntzrnm').text, soup.find('plntbnenm').text,
+        data = [id, name, soup.find('plntbnenm').text,
                 soup.find('adviseinfo').text, soup.find(
                     'growthhginfo').text, soup.find('growtharainfo').text,
-                soup.find('smellcodenm').text, soup.find(
+                soup.select_one('smellcode').text, soup.find(
                     'toxctyinfo').text, soup.find('managelevelcodenm').text,
                 soup.find('grwtvecodenm').text, soup.find(
-                    'grwhtpcodenm').text, soup.find('hdcodenm').text,
+                    'grwhtpcodenm').text, soup.select_one('hdcode').text,
                 soup.find('watercyclesprngcodenm').text, soup.find(
                     'watercyclewintercodenm').text,
                 soup.find('speclmanageinfo').text, soup.find(
@@ -82,8 +70,29 @@ for row in reader:
                     'lighttdemanddocodenm').text,
                 soup.find('postngplacecodenm').text, soup.find(
                     'dlthtscodenm').text, soup.find('grwhstlecodenm').text,
-                soup.find('winterlwettpcodenm').text]
+                soup.select_one('winterlwettpcode').text]
         writer.writerow(data)
+
+        # name
+        # data = [id, name, soup.find('plntbnenm').text,
+        #         soup.find('adviseinfo').text, soup.find(
+        #             'growthhginfo').text, soup.find('growtharainfo').text,
+        #         soup.find('smellcodenm').text, soup.find(
+        #             'toxctyinfo').text, soup.find('managelevelcodenm').text,
+        #         soup.find('grwtvecodenm').text, soup.find(
+        #             'grwhtpcodenm').text, soup.find('hdcodenm').text,
+        #         soup.find('watercyclesprngcodenm').text, soup.find(
+        #             'watercyclewintercodenm').text,
+        #         soup.find('speclmanageinfo').text, soup.find(
+        #             'fncltyinfo').text,
+        #         soup.find('managedemanddocodenm').text, soup.find(
+        #             'lefmrkcodenm').text,
+        #         soup.find('lefcolrcodenm').text, soup.find(
+        #             'lighttdemanddocodenm').text,
+        #         soup.find('postngplacecodenm').text, soup.find(
+        #             'dlthtscodenm').text, soup.find('grwhstlecodenm').text,
+        #         soup.find('winterlwettpcodenm').text]
+        # writer.writerow(data)
         cnt += 1
         print("UPDATED : %s : %d" % (id, cnt))
 
