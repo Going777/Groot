7a72622b09b7b3a9cedf9c49ad77498944430269
김정원 <=>
2023-04-28 15:40:28 +0900

    Update: 식물 컬렉션 버튼 이동 구현

--- front-end/groot/app/src/main/java/com/chocobi/groot/view/plant/PlantCollectionRVAdapter.kt
+++ front-end/groot/app/src/main/java/com/chocobi/groot/view/plant/PlantCollectionRVAdapter.kt
@@ -3,31 +3,61 @@ package com.chocobi.groot.view.plant
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.chocobi.groot.R
 
-class PlantCollectionRVAdapter(val items: MutableList<String>): RecyclerView.Adapter<PlantCollectionRVAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantCollectionRVAdapter.ViewHolder {
+
+class PlantCollectionRVAdapter(val items: MutableList<String>) :
+    RecyclerView.Adapter<PlantCollectionRVAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): PlantCollectionRVAdapter.ViewHolder {
         android.util.Log.d("로그", "PlantCollectionRVAdapter, $items")
-        var view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_plant_collection_item, parent, false)
+        var view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_plant_collection_item, parent, false)
 
         return ViewHolder(view)
     }
 
-//    interface ItemClick {
-//        fun onClick(view: View, position: Int)
-//    }
-//    var itemClick : ItemClick? = null
+    //    아이템 클릭 리스터 인터페이스
+    interface ItemClickListener {
+        fun onPostBtnClick(view: View, position: Int)
+        fun onScanBtnClick(view: View, position: Int)
+        fun onDetailBtnClick(view: View, position: Int)
+    }
+
+    private lateinit var postBtnClickListner: ItemClickListener
+    private lateinit var scanBtnClickListner: ItemClickListener
+    private lateinit var detailBtnClickListner: ItemClickListener
+    fun setItemClickListener(itemClickListener: ItemClickListener) {
+        this.postBtnClickListner = itemClickListener
+        this.scanBtnClickListner = itemClickListener
+        this.detailBtnClickListner = itemClickListener
+    }
+
 
     override fun onBindViewHolder(holder: PlantCollectionRVAdapter.ViewHolder, position: Int) {
-//        if ( itemClick != null) {
-//            holder.itemView.setOnClickListener {
-//                    v ->
-//                itemClick?.onClick(v, position)
-//            }
-//        }
         holder.bindItems(items[position])
+
+        val postBtn = holder.itemView.findViewById<Button>(R.id.plantPostDiaryBtn)
+        val scanBtn = holder.itemView.findViewById<Button>(R.id.plantScanBtn)
+        val detailBtn = holder.itemView.findViewById<Button>(R.id.plantDetailBtn)
+
+        postBtn.setOnClickListener {
+            postBtnClickListner.onPostBtnClick(it, position)
+        }
+
+        scanBtn.setOnClickListener {
+            scanBtnClickListner.onScanBtnClick(it, position)
+        }
+
+        detailBtn.setOnClickListener {
+            detailBtnClickListner.onDetailBtnClick(it, position)
+        }
     }
 
     //    전체 리사이클러뷰의 개수
@@ -37,10 +67,9 @@ class PlantCollectionRVAdapter(val items: MutableList<String>): RecyclerView.Ada
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        fun bindItems(item:String) {
+        fun bindItems(item: String) {
             val rv_text = itemView.findViewById<TextView>(R.id.plant_name)
             rv_text.text = item
-
         }
     }
 
