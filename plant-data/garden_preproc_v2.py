e86665db9efc7f6f024ab808039aca32b056d897
bkkmw <roboticts49@gmail.com>
2023-04-30 16:53:52 +0900

    Fix: garden preproc chatGPT temperature

--- plant-data/garden_preproc_v2.py
+++ plant-data/garden_preproc_v2.py
@@ -5,13 +5,16 @@ import openai
 from time import sleep
 
 
-def get_gpt_response(message):
+def get_gpt_response(content, num):
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": message}
-        ]
+            {"role": "assistant", "name": "trial4" +
+                str(num), "content": content}
+        ],
+        temperature=1.2
     )
+    print("RESPONSE !")
     print(completion.choices[0].message.content)
     return completion.choices[0].message.content
 
@@ -33,41 +36,51 @@ fr = open(filename, 'r', encoding='utf-8-sig')
 reader = csv.reader(fr)
 
 # output file
-filename = os.environ.get('OUTPUT_DIR') + 'garden_preproc_v2.csv'
+# filename = os.environ.get('OUTPUT_DIR') + 'garden_preproc_v2.csv'
 fw = open(filename, 'w', encoding='utf-8-sig', newline='')
+fw = open(filename, 'a', encoding='utf-8-sig', newline='')
 writer = csv.writer(fw)
 
 # output columns
-writer.writerow(col)
+# writer.writerow(col)
 
 # base URL
 openai.api_key = os.environ.get('CHATGPT_API_KEY')
 
 # read line
 cnt = 0
+api_cnt = 0
 for row in reader:
     if row[0] == 'id':
         continue
 
     cnt += 1
-    if cnt % 10 != 0:
-        continue
-
     data = row
-    message = "아래 내용 말투 예쁘게 바꿔서 한국어로 출력해줘.\n"
+
+    # convert via Chat GPT
+    message = "아래 내용의 문장을 친절하게 바꿔줘.\n"
     if data[15].strip() != '':
         tip_message = message + row[15]
-        data[15] = get_gpt_response(tip_message)
+        data[15] = get_gpt_response(tip_message, cnt)
+        api_cnt += 1
+
+    if api_cnt > 2:
+        print("INFO : API call limits! wait 1 minute")
+        api_cnt = 0
+        sleep(60)
 
     if data[23].strip() != '':
         desc_message = message + row[23]
-        data[23] = get_gpt_response(desc_message)
+        data[23] = get_gpt_response(desc_message, cnt)
+        api_cnt += 1
+
+    if api_cnt > 2:
+        print("INFO : API call limits! wait 1 minute")
+        api_cnt = 0
+        sleep(60)
 
     writer.writerow(data)
-    print("[%3d] row done : wait 1 minute" % (cnt))
-    # if cnt == 10:
-    #     break
-    sleep(50)
+    print("[%3d] row done : " % (cnt))
 
 
 print("DONE")
