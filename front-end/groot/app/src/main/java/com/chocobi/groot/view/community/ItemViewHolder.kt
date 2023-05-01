5bedfb576edb0c0961f04035ba997f87004c897a
sehee <parksehee6938@gmail.com>
2023-05-01 18:01:18 +0900

    Feat: article-list-api 연결

--- front-end/groot/app/src/main/java/com/chocobi/groot/view/community/ItemViewHolder.kt
+++ front-end/groot/app/src/main/java/com/chocobi/groot/view/community/ItemViewHolder.kt
@@ -10,12 +10,14 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.FutureTarget
 import com.chocobi.groot.R
 import com.chocobi.groot.Thread.ThreadUtil
+import com.chocobi.groot.view.community.model.CommunityArticleListResponse
+import org.w3c.dom.Text
 import java.lang.ref.WeakReference
 
 class ItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
     interface ItemViewHolderDelegate {
-        fun onItemViewClick(itemBean: ItemBean) {
+        fun onItemViewClick(communityArticleListResponse: CommunityArticleListResponse) {
             Log.d("ItemViewHolder", "clicked")
         }
     }
@@ -24,10 +26,16 @@ class ItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
     private lateinit var imageView: ImageView
     private lateinit var textViewTitle: TextView
-    private lateinit var textViewContent: TextView
+    private lateinit var textViewNickName: TextView
+    private lateinit var textViewTag: TextView
+    private lateinit var eyeCnt: TextView
+    private lateinit var commentCnt: TextView
+    private lateinit var booleanBookmark: TextView
+    private lateinit var createTime: TextView
+    private lateinit var img: ImageView
 
     var delegate: ItemViewHolderDelegate? = null
-    lateinit var itemBean: ItemBean
+    lateinit var communityArticleListResponse: CommunityArticleListResponse
 
     init {
         findView()
@@ -38,30 +46,41 @@ class ItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         view.get()?.let {
             imageView = it.findViewById(R.id.imageView)
             textViewTitle = it.findViewById(R.id.textViewTitle)
-            textViewContent = it.findViewById(R.id.textViewContent)
+            textViewNickName = it.findViewById(R.id.textViewWriter)
+            textViewTag = it.findViewById(R.id.textViewTag)
+            eyeCnt = it.findViewById(R.id.eyeCnt)
+            commentCnt = it.findViewById(R.id.commentCnt)
+            booleanBookmark= it.findViewById(R.id.booleanBookmark)
+            createTime= it.findViewById(R.id.createTime)
+            img = it.findViewById(R.id.imageView)
+
         }
     }
 
     private fun setListener() {
         view.get()?.setOnClickListener {
-            delegate?.onItemViewClick(itemBean)
+            delegate?.onItemViewClick(communityArticleListResponse)
         }
     }
 
     fun updateView() {
-        textViewTitle.text = itemBean.title
-        textViewContent.text = itemBean.content
+        textViewTitle.text = communityArticleListResponse.articles.content[0].title
+        textViewNickName.text = communityArticleListResponse.articles.content[0].nickName
+        textViewTag.text = communityArticleListResponse.articles.content[0].tags.toString()
+        eyeCnt.text = communityArticleListResponse.articles.content[0].views.toString()
+        commentCnt.text = communityArticleListResponse.articles.content[0].commentCnt.toString()
+        booleanBookmark.text = communityArticleListResponse.articles.content[0].bookmark.toString()
+        createTime.text = communityArticleListResponse.articles.content[0].createTime.toString()
+
 
         imageView.post {
-
-//            val imageUrl = "https://cdn.wallpapersafari.com/15/87/kp4wAJ.jpg"
 
             view.get()?.let {
 
                 ThreadUtil.startThread {
                     val futureTarget: FutureTarget<Bitmap> = Glide.with(it.context)
                         .asBitmap()
-                        .load(itemBean.imageUrl)
+                        .load(communityArticleListResponse.articles.content.getOrNull(0)?.img)
                         .submit(imageView.width, imageView.height)
 
                     val bitmap = futureTarget.get()
@@ -70,11 +89,9 @@ class ItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
                         imageView.setImageBitmap(bitmap)
                     }
                 }
-
-
-
             }
         }
+
 
     }
 
