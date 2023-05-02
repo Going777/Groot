e410f65487f07d4e72ec850c0bcdb8608154a4f3
shcho <tmdgml1754@naver.com>
2023-05-02 23:59:19 +0900

    Fix: QEntity overwritten 해결

---
+++ back-end/backend/src/main/generated/com/groot/backend/entity/QTagEntity.java
@@ -0,0 +1,39 @@
+package com.groot.backend.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QTagEntity is a Querydsl query type for TagEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QTagEntity extends EntityPathBase<TagEntity> {
+
+    private static final long serialVersionUID = -1307528610L;
+
+    public static final QTagEntity tagEntity = new QTagEntity("tagEntity");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public QTagEntity(String variable) {
+        super(TagEntity.class, forVariable(variable));
+    }
+
+    public QTagEntity(Path<? extends TagEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QTagEntity(PathMetadata metadata) {
+        super(TagEntity.class, metadata);
+    }
+
+}
+
