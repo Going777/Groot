9adcc209456f4593c6b98bcfbdef5e0b199a154e
Dakyung Seo <seoda0000@gmail.com>
2023-05-02 09:23:00 +0900

    Refactor: SignupActivity

--- front-end/groot/app/src/main/java/com/chocobi/groot/view/signup/SignupActivity.kt
+++ front-end/groot/app/src/main/java/com/chocobi/groot/view/signup/SignupActivity.kt
@@ -39,104 +39,18 @@ class SignupActivity : AppCompatActivity() {
         var basicSignupBtn = findViewById<Button>(R.id.basicSignupBtn)
 
 
-        //        retrofit 객체 만들기
-        var retrofit = Retrofit.Builder()
-            .baseUrl(GlobalVariables.getBaseUrl())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-
-        //        id 중복체크
-        //        service 객체 만들기
-        var dupIdService = retrofit.create(DupIdService::class.java)
         dupIdBtn.setOnClickListener {
             var textId = signupIdInput.text.toString()
-            dupIdService.requestDupId(textId)
-                .enqueue(object : Callback<DupIdResponse> {
-                    override fun onResponse(
-                        call: Call<DupIdResponse>,
-                        response: Response<DupIdResponse>
-                    ) {
-                        var checkDupId = response.body()?.msg
-                        if (checkDupId == null) {
-                            checkDupId =
-                                JSONObject(response.errorBody()?.string()!!).getString("msg")
-                        } else {
-                            isCheckedDupId = true
-                        }
-                        var dialog = AlertDialog.Builder(
-                            this@SignupActivity,
-                            android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth
-                        )
-                        Log.d("SignupActivity", response.toString())
-                        dialog.setTitle("아이디 중복 체크")
-                        dialog.setMessage(checkDupId)
-                        dialog.setPositiveButton(
-                            "확인",
-                            DialogInterface.OnClickListener { dialog, which ->
-                                dialog.dismiss()
-                            })
-                        dialog.show()
-                    }
-
-                    override fun onFailure(call: Call<DupIdResponse>, t: Throwable) {
-                        var dialog = AlertDialog.Builder(this@SignupActivity)
-                        dialog.setTitle("아이디 중복체크 실패")
-                        dialog.setMessage(t.message)
-                        dialog.show()
-                    }
-                })
-
+            checkDupId(textId)
         }
 
 
-        //        닉네임 중복체크
-        //        service 객체 만들기
-        var dupNameService = retrofit.create(DupNameService::class.java)
         dupNameBtn.setOnClickListener {
             var textName = signupNameInput.text.toString()
-
-            dupNameService.requestDupName(textName)
-                .enqueue(object : Callback<DupNameResponse> {
-                    override fun onResponse(
-                        call: Call<DupNameResponse>,
-                        response: Response<DupNameResponse>
-                    ) {
-                        var checkDupName = response.body()?.msg
-                        if (checkDupName == null) {
-                            checkDupName =
-                                JSONObject(response.errorBody()?.string()!!).getString("msg")
-                        } else {
-                            isCheckedDupName = true
-                        }
-                        var dialog = AlertDialog.Builder(
-                            this@SignupActivity,
-                            android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth
-                        )
-                        Log.d("SignupActivity", response.toString())
-                        dialog.setTitle("닉네임 중복 체크")
-                        dialog.setMessage(checkDupName)
-                        dialog.setPositiveButton(
-                            "확인",
-                            DialogInterface.OnClickListener { dialog, which ->
-                                dialog.dismiss()
-                            })
-                        dialog.show()
-                    }
-
-                    override fun onFailure(call: Call<DupNameResponse>, t: Throwable) {
-                        var dialog = AlertDialog.Builder(this@SignupActivity)
-                        dialog.setTitle("닉네임 중복체크 실패")
-                        dialog.setMessage(t.message)
-                        dialog.show()
-                    }
-                })
+            checkDupName(textName)
         }
 
 
-//        회원가입
-        //        service 객체 만들기
-        var signupService = retrofit.create(SignupService::class.java)
         basicSignupBtn.setOnClickListener {
             var textId = signupIdInput.text.toString()
             var textName = signupNameInput.text.toString()
@@ -165,65 +79,163 @@ class SignupActivity : AppCompatActivity() {
                 }
 
                 else -> {
-                    signupService.requestSignup(SignupRequest(textId, textName, textPw))
-                        .enqueue(object : Callback<SignupResponse> {
-                            override fun onResponse(
-                                call: Call<SignupResponse>,
-                                response: Response<SignupResponse>
-                            ) {
-                                var signup = response.body()
-                                var signupMsg = signup?.msg
-                                if (response.code() == 200) {
-
-
-                                    //                    토큰 저장
-                                    editor.putString("access_token", signup?.accessToken)
-//                      editor.putString("refresh_token", login?.refreshToken)
-                                    editor.commit()
-
-
-                                    var dialog = AlertDialog.Builder(
-                                        this@SignupActivity,
-                                        android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth
-                                    )
-
-                                    dialog.setTitle("환영합니다!")
-                                    dialog.setMessage(signupMsg)
-                                    dialog.setPositiveButton(
-                                        "확인",
-                                        DialogInterface.OnClickListener { dialog, which ->
-                                            var intent =
-                                                Intent(
-                                                    this@SignupActivity,
-                                                    MainActivity::class.java
-                                                )
-                                            startActivity(intent)
-                                        })
-                                    dialog.show()
-                                } else {
-                                    signupMsg =
-                                        JSONObject(
-                                            response.errorBody()?.string()!!
-                                        ).getString("msg")
-                                    Log.d("SignupActivity", response.code().toString())
-                                }
-
-
-                            }
-
-                            override fun onFailure(call: Call<SignupResponse>, t: Throwable) {
-                                var dialog = AlertDialog.Builder(this@SignupActivity)
-                                dialog.setTitle("회원가입 실패")
-                                dialog.setMessage(t.message)
-                                dialog.show()
-                            }
-                        })
+                    signup(textId, textName, textPw)
                 }
             }
-
-
         }
+    }
 
+    private fun checkDupId(textId: String) {
+        var retrofit = Retrofit.Builder()
+            .baseUrl(GlobalVariables.getBaseUrl())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        //        id 중복체크
+        //        service 객체 만들기
+        var dupIdService = retrofit.create(DupIdService::class.java)
+
+        dupIdService.requestDupId(textId)
+            .enqueue(object : Callback<DupIdResponse> {
+                override fun onResponse(
+                    call: Call<DupIdResponse>,
+                    response: Response<DupIdResponse>
+                ) {
+                    var checkDupIdMsg = response.body()?.msg
+                    if (checkDupIdMsg == null) {
+                        checkDupIdMsg =
+                            JSONObject(response.errorBody()?.string()!!).getString("msg")
+                    } else {
+                        isCheckedDupId = true
+                    }
+                    var dialog = AlertDialog.Builder(
+                        this@SignupActivity,
+                        android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth
+                    )
+                    Log.d("SignupActivity", response.toString())
+                    dialog.setTitle("아이디 중복 체크")
+                    dialog.setMessage(checkDupIdMsg)
+                    dialog.setPositiveButton(
+                        "확인",
+                        DialogInterface.OnClickListener { dialog, which ->
+                            dialog.dismiss()
+                        })
+                    dialog.show()
+                }
+
+                override fun onFailure(call: Call<DupIdResponse>, t: Throwable) {
+                    var dialog = AlertDialog.Builder(this@SignupActivity)
+                    dialog.setTitle("아이디 중복체크 실패")
+                    dialog.setMessage(t.message)
+                    dialog.show()
+                }
+            })
+
+    }
+
+    private fun checkDupName(textName:String) {
+        //        retrofit 객체 만들기
+        var retrofit = Retrofit.Builder()
+            .baseUrl(GlobalVariables.getBaseUrl())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        var dupNameService = retrofit.create(DupNameService::class.java)
+        dupNameService.requestDupName(textName)
+            .enqueue(object : Callback<DupNameResponse> {
+                override fun onResponse(
+                    call: Call<DupNameResponse>,
+                    response: Response<DupNameResponse>
+                ) {
+                    var checkDupName = response.body()?.msg
+                    if (checkDupName == null) {
+                        checkDupName =
+                            JSONObject(response.errorBody()?.string()!!).getString("msg")
+                    } else {
+                        isCheckedDupName = true
+                    }
+                    var dialog = AlertDialog.Builder(
+                        this@SignupActivity,
+                        android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth
+                    )
+                    Log.d("SignupActivity", response.toString())
+                    dialog.setTitle("닉네임 중복 체크")
+                    dialog.setMessage(checkDupName)
+                    dialog.setPositiveButton(
+                        "확인",
+                        DialogInterface.OnClickListener { dialog, which ->
+                            dialog.dismiss()
+                        })
+                    dialog.show()
+                }
+
+                override fun onFailure(call: Call<DupNameResponse>, t: Throwable) {
+                    var dialog = AlertDialog.Builder(this@SignupActivity)
+                    dialog.setTitle("닉네임 중복체크 실패")
+                    dialog.setMessage(t.message)
+                    dialog.show()
+                }
+            })
+
+    }
+
+    private fun signup(textId: String, textName: String, textPw: String) {
+        //        retrofit 객체 만들기
+        var retrofit = Retrofit.Builder()
+            .baseUrl(GlobalVariables.getBaseUrl())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        var signupService = retrofit.create(SignupService::class.java)
+
+        signupService.requestSignup(SignupRequest(textId, textName, textPw))
+            .enqueue(object : Callback<SignupResponse> {
+                override fun onResponse(
+                    call: Call<SignupResponse>,
+                    response: Response<SignupResponse>
+                ) {
+                    var signupBody = response.body()
+                    var signupMsg = signupBody?.msg
+                    if (response.code() == 200 && signupBody?.accessToken != null) {
+
+
+                        //                    토큰 저장
+                        GlobalVariables.prefs.setString("access_token", signupBody.accessToken)
+
+
+                        var dialog = AlertDialog.Builder(
+                            this@SignupActivity,
+                            android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth
+                        )
+
+                        dialog.setTitle("환영합니다!")
+                        dialog.setMessage(signupMsg)
+                        dialog.setPositiveButton(
+                            "확인",
+                            DialogInterface.OnClickListener { dialog, which ->
+                                var intent =
+                                    Intent(
+                                        this@SignupActivity,
+                                        MainActivity::class.java
+                                    )
+                                startActivity(intent)
+                            })
+                        dialog.show()
+                    } else {
+                        signupMsg =
+                            JSONObject(
+                                response.errorBody()?.string()!!
+                            ).getString("msg")
+                        Log.d("SignupActivity", response.code().toString() + signupMsg)
+                    }
+
+
+                }
+
+                override fun onFailure(call: Call<SignupResponse>, t: Throwable) {
+                    var dialog = AlertDialog.Builder(this@SignupActivity)
+                    dialog.setTitle("회원가입 실패")
+                    dialog.setMessage(t.message)
+                    dialog.show()
+                }
+            })
 
     }
 }
