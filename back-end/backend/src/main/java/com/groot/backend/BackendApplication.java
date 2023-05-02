95df5e2a8e0fa539a7c5a8ef73d25c562a7ed874
ymj3539 <ymj3539@naver.com>
2023-05-03 00:46:59 +0900

    Fix: fix backend build error

--- back-end/backend/src/main/java/com/groot/backend/BackendApplication.java
+++ back-end/backend/src/main/java/com/groot/backend/BackendApplication.java
@@ -2,10 +2,9 @@ package com.groot.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableCaching
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class BackendApplication {
