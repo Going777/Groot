235787dac1f7831ce989eca9ea20ef8f0293ff06
김정원 <=>
2023-05-04 17:55:20 +0900

    Update: 지역필터 설정 버튼 연결 중

--- front-end/groot/app/src/main/java/com/chocobi/groot/view/intro/IntroDataResponse.kt
+++ front-end/groot/app/src/main/java/com/chocobi/groot/view/intro/IntroDataResponse.kt
@@ -4,3 +4,9 @@ data class PlantNamesResponse(
     val msg: String,
     val nameList: MutableList<String>
 )
+
+data class RegionNameResponse(
+    val msg: String,
+    val result: String,
+    val regions: MutableList<String>
+)
