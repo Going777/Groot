f15cdfd5f70f4d07bcf6568b8835d4e66bcbd7bc
김정원 <wjddnjs0528@gmail.com>
2023-05-10 00:15:53 +0900

    Update: 화분 생성 캐릭터 애니메이션 추가 및 레이아웃 조정

--- front-end/groot/app/src/main/java/com/chocobi/groot/view/addpot/Pot1Activity.kt
+++ front-end/groot/app/src/main/java/com/chocobi/groot/view/addpot/Pot1Activity.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.utils.widget.ImageFilterButton
 import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 import com.chocobi.groot.R
@@ -81,7 +82,7 @@ class Pot1Activity : AppCompatActivity() {
                     Chip(this, null, R.style.REGION_CHIP_ICON).apply {
                         text = type
                         setTextColor(ContextCompat.getColor(context, R.color.white))
-                        setChipBackgroundColorResource(R.color.main)
+                        setChipBackgroundColorResource(R.color.sub)
                         chipStrokeWidth = 0f
                     }
                 )
@@ -89,15 +90,15 @@ class Pot1Activity : AppCompatActivity() {
         }
 
 
-        Log.d("Pot1Activity", "onCreate() $growType")
-        Log.d("Pot1Activity", "onCreate() $mgmtLevel")
-        Log.d("Pot1Activity", "onCreate() $characterGlbPath")
+        Log.d("Pot1Activity", "onCreate() 111 $growType")
+        Log.d("Pot1Activity", "onCreate() 111 $mgmtLevel")
+        Log.d("Pot1Activity", "onCreate() 111 $characterGlbPath")
 
         //        image 띄우기
         var potPhoto = findViewById<ImageView>(R.id.potPhoto)
         potPhoto.setImageURI(imageUri?.toUri())
 
-        val add1Btn = findViewById<Button>(R.id.add1Btn)
+        val add1Btn = findViewById<ImageFilterButton>(R.id.add1Btn)
         add1Btn.setOnClickListener {
             var intent = Intent(this, Pot2Activity::class.java)
 //            intent.putExtra("toPage", "plant_add1")
@@ -105,7 +106,6 @@ class Pot1Activity : AppCompatActivity() {
             intent.putExtra("plantNameSplit", plantNameSplit)
             intent.putExtra("plantId", plantId)
             intent.putExtra("imageUri", imageUri)
-
             intent.putExtra("growType", growType)
             intent.putExtra("mgmtLevel", mgmtLevel)
             intent.putExtra("characterGlbPath", characterGlbPath)
--- front-end/groot/app/src/main/java/com/chocobi/groot/view/addpot/Pot2Activity.kt
+++ front-end/groot/app/src/main/java/com/chocobi/groot/view/addpot/Pot2Activity.kt
@@ -1,22 +1,33 @@
 package com.chocobi.groot.view.addpot
 
+import android.animation.Animator
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.net.toUri
+import com.airbnb.lottie.LottieAnimationView
 import com.chocobi.groot.MainActivity
 import com.chocobi.groot.R
 import com.chocobi.groot.data.RetrofitClient
 import com.chocobi.groot.view.addpot.model.AddPotRequest
 import com.chocobi.groot.view.addpot.model.AddPotResponse
 import com.chocobi.groot.view.addpot.model.AddPotService
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.google.ar.sceneform.lullmodel.VertexAttributeUsage.Position
 import io.github.sceneview.SceneView
 import io.github.sceneview.node.ModelNode
@@ -30,6 +41,7 @@ import java.io.File
 import java.io.FileOutputStream
 import io.github.sceneview.utils.Color
 import io.github.sceneview.math.Position
+import io.github.sceneview.math.Rotation
 
 class Pot2Activity : AppCompatActivity() {
     private val TAG = "Pot2Activity"
@@ -37,7 +49,13 @@ class Pot2Activity : AppCompatActivity() {
     private var plantNameSplit: String = ""
     private var isSuccessed = false
 
+    private lateinit var scrollView: ScrollView
     private lateinit var characterSceneView: SceneView
+    private lateinit var lottieView: LottieAnimationView
+    private lateinit var potNameLayout: TextInputLayout
+    private lateinit var potNameTextView: TextView
+    private lateinit var potNameEdit: TextInputEditText
+
     private var growType: String? = null
     private var mgmtLevel: String? = null
     private var characterGlbPath: String? = null
@@ -50,57 +68,90 @@ class Pot2Activity : AppCompatActivity() {
         plantNameSplit = intent.getStringExtra("plantNameSplit").toString()
         plantId = intent.getIntExtra("plantId", 0)
         val tempPotName = intent.getStringExtra("tempPotName")
+//        val tempPotName = "루티"
         var imageUri = intent.getStringExtra("imageUri")
 
         var plantNameShort = findViewById<TextView>(R.id.plantNameShort)
-        var potNameText = findViewById<TextView>(R.id.potNameText)
-        val potNameEdit = findViewById<EditText>(R.id.potNameEdit)
+        potNameTextView = findViewById<TextView>(R.id.potNameText)
+        potNameEdit = findViewById<TextInputEditText>(R.id.potNameEdit)
 
         plantNameShort.text = plantNameSplit
-        potNameText.text = tempPotName
-        potNameEdit.setText(tempPotName)
+//        potNameText.text = tempPotName
+//        potNameEdit.setText(tempPotName)
 
 
+        scrollView = findViewById(R.id.scrollView)
+        characterSceneView = findViewById(R.id.characterSceneView)
+        lottieView = findViewById(R.id.lottieView)
         growType = intent.getStringExtra("growType")
         mgmtLevel = intent.getStringExtra("mgmtLevel")
         characterGlbPath = intent.getStringExtra("characterGlbPath")
+
+//        키보드 올라왔을 때 그만큼 스크롤 올리기
+        potNameEdit.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                scrollView.post {
+                    scrollView.scrollTo(0, potNameEdit.bottom)
+                }
+            }
+        }
+
+        lottieView.setAnimation(R.raw.congratulation)
         setCharacterSceneView()
 
 
         //        화분 등록 및 Plant Detail 페이지로 이동
-        val add2Btn = findViewById<Button>(R.id.add2Btn)
+        val add2Btn = findViewById<ImageButton>(R.id.add2Btn)
         add2Btn.setOnClickListener {
-            Log.d(TAG, plantId.toString())
-            Log.d(TAG, potNameEdit.text.toString())
-            Log.d(TAG, imageUri.toString())
-            if (plantId > 0 && potNameEdit.text.toString() != "" && imageUri != null) {
+            val potName = potNameEdit.text.toString()
+            if (potName == "") {
+                Toast.makeText(this, "루티에게 이름을 지어주세요", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.d(TAG, plantId.toString())
+                Log.d(TAG, potName)
+                Log.d(TAG, imageUri.toString())
+                if (plantId > 0 && potName != "" && imageUri != null) {
 
-                var file = uriToFile(imageUri.toUri())
-                addPot(this, plantId, potNameEdit.text.toString(), file)
-
-
+                    var file = uriToFile(imageUri.toUri())
+                    addPot(this, plantId, potNameEdit.text.toString(), file)
+                }
             }
         }
-
-        characterSceneView = findViewById(R.id.characterSceneView)
-
     }
 
     private fun setCharacterSceneView() {
+
+        Log.d("Pot1Activity", "onCreate() 222 $growType")
+        Log.d("Pot1Activity", "onCreate() 222 $mgmtLevel")
+        Log.d("Pot1Activity", "onCreate() 222 $characterGlbPath")
+
         if (modelNode != null) {
             characterSceneView.removeChild(modelNode!!)
         }
 
-        characterSceneView.backgroundColor = Color(255.0f, 255.0f, 255.0f, 255.0f)
+        characterSceneView.backgroundColor = Color(255.0f, 255.0f, 255.0f, 0.0f)
 
         modelNode = ModelNode().apply {
             loadModelGlbAsync(
                 glbFileLocation = characterGlbPath
                     ?: "https://groot-a303-s3.s3.ap-northeast-2.amazonaws.com/assets/unicorn_2.glb",
                 autoAnimate = false,
-                scaleToUnits = 1.0f,
-                centerOrigin = Position(x = 0f, y = 0f, z = 0f),
-            )
+                scaleToUnits = 0.8f,
+                centerOrigin = Position(x = 0f, y = 1f, z = 0f),
+            ) {
+                isScaleEditable = true
+                isRotationEditable = false
+                maxEditableScale = 1f
+                minEditableScale = 0.5f
+                rotation = Rotation(30.0f, 0.0f, 0.0f)
+                playAnimation(15, true)
+
+                Handler().postDelayed({
+                    stopAnimation(15)
+                    lottieView.playAnimation()
+                    playAnimation(18, true)
+                }, 1500)
+            }
         }
         if (modelNode != null) {
             characterSceneView.addChild(modelNode!!)
@@ -151,7 +202,11 @@ class Pot2Activity : AppCompatActivity() {
                     Log.d(TAG, "$body")
                     if (body != null) {
                         body.potId
-                        Toast.makeText(context, body.potId.toString() + "번 화분이 등록 되었습니다.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            context,
+                            body.potId.toString() + "번 화분이 등록 되었습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
                         var intent = Intent(context, MainActivity::class.java)
                         intent.putExtra("toPage", "pot_detail")
                         intent.putExtra("potId", body.potId)
