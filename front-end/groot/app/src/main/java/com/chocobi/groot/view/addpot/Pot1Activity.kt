3e617c1e7d6e5d2d0879409b02d15f556b3a1ebc
김정원 <=>
2023-05-12 10:05:00 +0900

    Fix: 식물등록 시 chip 오류 해결

--- front-end/groot/app/src/main/java/com/chocobi/groot/view/addpot/Pot1Activity.kt
+++ front-end/groot/app/src/main/java/com/chocobi/groot/view/addpot/Pot1Activity.kt
@@ -74,9 +74,9 @@ class Pot1Activity : AppCompatActivity() {
 //        }
 
 
-        Log.d("Pot1Activity","onCreate() 숙련도 $mgmtLevel")
-        Log.d("Pot1Activity","onCreate() 숙련도 $growTypes")
-        if (mgmtLevel != "" || mgmtLevel != null ) {
+        Log.d("Pot1Activity","onCreate() 숙련도 ${mgmtLevel}")
+        Log.d("Pot1Activity","onCreate() 생장 ${growTypes}")
+        if (mgmtLevel != "") {
             potInfochipGroup.addView(
                 Chip(this, null, R.style.REGION_CHIP_ICON).apply {
                     text = mgmtLevel
