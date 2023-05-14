83cee6fdc584cbbdb5c645ae3fb522de0c6a3da1
bkkmw <roboticts49@gmail.com>
2023-05-14 17:27:50 +0900

    Feat: searching from another project

--- back-end/backend/src/main/java/com/groot/backend/service/PlantServiceImpl.java
+++ back-end/backend/src/main/java/com/groot/backend/service/PlantServiceImpl.java
@@ -48,8 +48,8 @@ public class PlantServiceImpl implements PlantService{
     @Value("${plantnet.apiKey}")
     private String plantNetApiKey;
 
-    private String plantNetUrl = "https://my-api.plantnet.org/v2/identify/all?include-related-images=false&no-reject=false&lang=en&api-key=";
-//    private String plantNetUrl = "https://my-api.plantnet.org/v2/identify/prosea?include-related-images=false&no-reject=false&lang=en&api-key=";
+//    private String plantNetUrl = "https://my-api.plantnet.org/v2/identify/all?include-related-images=false&no-reject=false&lang=en&api-key=";
+    private String plantNetUrl = "https://my-api.plantnet.org/v2/identify/prosea?include-related-images=false&no-reject=false&lang=en&api-key=";
 
     @Override
     public List<String> getNameList() {
