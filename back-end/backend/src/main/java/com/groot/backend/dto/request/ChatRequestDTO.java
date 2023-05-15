3056731fcc10b3657f5e9f8bb84043af3ccafba4
seunghee <tmdgml1754@naver.com>
2023-05-15 15:26:39 +0900

    Feat: chatting create, read, delete

---
+++ back-end/backend/src/main/java/com/groot/backend/dto/request/ChatRequestDTO.java
@@ -0,0 +1,11 @@
+package com.groot.backend.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ChatRequestDTO {
+    private Long userPK;
+    private Long roomId;
+}
---
+++ back-end/backend/src/main/java/com/groot/backend/dto/response/ChatResponseDTO.java
@@ -0,0 +1,13 @@
+package com.groot.backend.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ChatResponseDTO {
+    private Long userPK;
+    private String nickName;
+    private String profile;
+    private Long roomId;
+}
