eed59d1079711c8503a39facd415b5978be84738
Dakyung Seo <seoda0000@gmail.com>
2023-05-25 09:27:42 +0900

    Refactor: 코드 정리 및 주석 추가

--- front-end/groot/app/src/main/java/com/chocobi/groot/view/sensor/SensorActivity.kt
+++ front-end/groot/app/src/main/java/com/chocobi/groot/view/sensor/SensorActivity.kt
@@ -11,6 +11,7 @@ import android.hardware.SensorManager
 import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.chocobi.groot.data.RetrofitClient
 import com.chocobi.groot.view.sensor.model.SensorResponse
@@ -30,55 +31,53 @@ class SensorActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var envStatusInfo: TextView
     private var plantId: Int = 0
     private var plantName: String = ""
-    private var maxLux : Int = 0
-    private var minLux : Int = 0
+    private var maxLux: Int = 0
+    private var minLux: Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sensor)
 
+//        뒤로가기 적용
         val backBtn = findViewById<ImageView>(R.id.backBtn)
         backBtn.setOnClickListener {
             this.onBackPressed()
         }
 
+        findViews()
         plantId = intent.getIntExtra("plantId", 0)
         val plantName = intent.getStringExtra("plantName")
         val plantNameText = findViewById<TextView>(R.id.plantName)
         plantNameText.text = plantName
-        lightValueText = findViewById(R.id.lightValueText)
-        envStatus = findViewById(R.id.envStatus)
-        envStatusInfo = findViewById(R.id.envStatusInfo)
+
+        //    식물별 조도 데이터 불러오기
+        getPlantLux(this)
+
 
         // 센서 매니저 인스턴스 생성
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        var deviceSensors: List<Sensor> = sensorManager!!.getSensorList(Sensor.TYPE_ALL)
-
-        for (i in deviceSensors) {
-        Log.d(TAG, "deviceSensor: $i")
-        }
-
-        getPlantLux()
-
 
         // 조도 센서 인스턴스 생성
         lightSensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
 
-
         // 센서 등록
         sensorManager!!.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
- }
+    }
+
+    private fun findViews() {
+        lightValueText = findViewById(R.id.lightValueText)
+        envStatus = findViewById(R.id.envStatus)
+        envStatusInfo = findViewById(R.id.envStatusInfo)
+    }
 
     override fun onSensorChanged(event: SensorEvent?) {
         if (event == null) return
-//        Log.d(
-//            "Sensor",
-//            "onSensorChanged(): sensorType=${event.sensor.type}, values=${event.values.joinToString()}"
-//        )
 
-        // 센서 타입 확인
+//        센서 타입 확인
         when (event.sensor.type) {
+
+//            광도 센서 : 광도에 따라 화면 변화
             Sensor.TYPE_LIGHT -> {
                 val lightValue = event.values[0]
                 lightValueText.text = lightValue.toString()
@@ -95,6 +94,8 @@ class SensorActivity : AppCompatActivity(), SensorEventListener {
                     envStatusInfo.text = "식물을 키우기에 너무 어두워요"
                     envStatus.setTextColor(ContextCompat.getColor(this, R.color.bug))
                     envStatusInfo.setTextColor(ContextCompat.getColor(this, R.color.bug))
+
+
                 } else {
                     envStatus.text = "부적합"
                     envStatusInfo.text = "식물을 키우기에 너무 밝아요"
@@ -105,9 +106,9 @@ class SensorActivity : AppCompatActivity(), SensorEventListener {
         }
     }
 
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        // 필요 없는 코드
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
     }
+
 
     override fun onPause() {
         super.onPause()
@@ -118,14 +119,17 @@ class SensorActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onResume() {
         super.onResume()
+
         // 센서 등록
         sensorManager!!.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
- }
+    }
 
-    private fun getPlantLux() {
+
+
+    private fun getPlantLux(context: Context) {
         var retrofit = RetrofitClient.getClient()!!
         var sensorService = retrofit.create(SensorService::class.java)
-        sensorService.getPlantLux(plantId).enqueue(object :Callback<SensorResponse> {
+        sensorService.getPlantLux(plantId).enqueue(object : Callback<SensorResponse> {
             override fun onResponse(
                 call: Call<SensorResponse>,
                 response: Response<SensorResponse>
@@ -138,7 +142,7 @@ class SensorActivity : AppCompatActivity(), SensorEventListener {
             }
 
             override fun onFailure(call: Call<SensorResponse>, t: Throwable) {
-                TODO("Not yet implemented")
+                Toast.makeText(context, "네트워크 오류입니다. 잠시 후 다시 시도해주세요.", Toast.LENGTH_LONG).show()
             }
         })
     }
